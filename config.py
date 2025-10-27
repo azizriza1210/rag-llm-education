cb1e51255f3d5f7c73779a78a61b1bc936415443
mazizr <saitamasensei710@gmail.com>
2025-10-27 08:26:11 +0700

    modify models_utils.py

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+HUGGINGFACE_API_KEY = os.getenv("HUGGINGFACE_API_KEY")
 
 CHROMA_PERSIST_DIRECTORY = "chroma_db"
 COLLECTION_NAME = "machine_learning_modules"
--- src/utils/model_utils.py
+++ src/utils/model_utils.py
@@ -1,6 +1,7 @@
 from langchain_groq import ChatGroq
 from langchain_community.embeddings import HuggingFaceEmbeddings
-from config import GROQ_API_KEY
+from config import GROQ_API_KEY, HUGGINGFACE_API_KEY
+import os
 
 # def get_llm(GROQ_API_KEY):
 llm = ChatGroq(
@@ -20,9 +21,14 @@ llm = ChatGroq(
 # embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 
 # ✅ Model kecil (~25MB)
-embeddings = HuggingFaceEmbeddings(
-    model_name="models/all-MiniLM-L6-v2/snapshots/c9745ed1d9f207416be6d2e6f8de32d1f16199bf",
-    model_kwargs={'device': 'cpu'},
-    encode_kwargs={'normalize_embeddings': True}
+# embeddings = HuggingFaceEmbeddings(
+#     model_name="models/all-MiniLM-L6-v2/snapshots/c9745ed1d9f207416be6d2e6f8de32d1f16199bf",
+#     model_kwargs={'device': 'cpu'},
+#     encode_kwargs={'normalize_embeddings': True}
+# )
+from langchain_huggingface import HuggingFaceEndpointEmbeddings
+embeddings = HuggingFaceEndpointEmbeddings(
+    model="sentence-transformers/all-MiniLM-L6-v2",
+    huggingfacehub_api_token=HUGGINGFACE_API_KEY
 )
     # return embeddings
