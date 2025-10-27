c6ca933c9e5590c868cabfefe287b170643ab436
mazizr <saitamasensei710@gmail.com>
2025-10-27 07:35:06 +0700

    modify models_utils.py

--- src/utils/model_utils.py
+++ src/utils/model_utils.py
@@ -14,7 +14,15 @@ llm = ChatGroq(
 # embeddings = HuggingFaceEmbeddings(
 #     model_name="sentence-transformers/all-MiniLM-L6-v2"
 # )
+# embeddings = HuggingFaceEmbeddings(
+#     model_name="models/all-MiniLM-L6-v2/snapshots/c9745ed1d9f207416be6d2e6f8de32d1f16199bf"
+# )
+# embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+
+# ✅ Model kecil (~25MB)
 embeddings = HuggingFaceEmbeddings(
-    model_name="models/all-MiniLM-L6-v2/snapshots/c9745ed1d9f207416be6d2e6f8de32d1f16199bf"
+    model_name="models/all-MiniLM-L6-v2/snapshots/c9745ed1d9f207416be6d2e6f8de32d1f16199bf",
+    model_kwargs={'device': 'cpu'},
+    encode_kwargs={'normalize_embeddings': True}
 )
     # return embeddings
